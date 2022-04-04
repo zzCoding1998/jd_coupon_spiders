99b4e06fae6cdf3eee8be6b84167e27528cf2b69
ZhuoZHang <zhuo.zhang@i9i8.com>
2022-04-04 17:08:53 +0800

    # 测试

---
+++ sh/crawl_jd_coupon.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+LOCK_FILE=/tmp/crawl_jd_coupon.lock
+exec 99>"$LOCK_FILE"
+flock -n 99
+if [ "$?" != 0 ]; then
+    echo "$0 already running"
+    exit 1
+fi
+cd /app && scrapy crawl jdCouponSpider
